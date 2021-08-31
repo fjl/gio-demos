9a2d5792fe7676885a9b9f193f3c2465a856c889
Felix Lange <fjl@twurst.com>
2021-08-31 17:54:51 +0200

    giotodo: improve store latency

--- giotodo/internal/todostore/todostore.go
+++ giotodo/internal/todostore/todostore.go
@@ -1,6 +1,7 @@
 package todostore
 
 import (
+	"container/list"
 	crand "crypto/rand"
 	"encoding/hex"
 	"encoding/json"
@@ -34,9 +35,10 @@ type Store struct {
 	writer   *json.Encoder
 
 	eventsOut  chan Event
-	eventQueue []Event
+	eventQueue list.List
 
 	eventsIn chan Event
+	flushCh  chan struct{}
 	quitCh   chan struct{}
 	wg       sync.WaitGroup
 }
@@ -44,8 +46,9 @@ type Store struct {
 func NewStore(datadir string) *Store {
 	s := &Store{
 		dataDir:   datadir,
-		eventsOut: make(chan Event, 256),
-		eventsIn:  make(chan Event),
+		eventsOut: make(chan Event),
+		eventsIn:  make(chan Event, 256),
+		flushCh:   make(chan struct{}, 1),
 		quitCh:    make(chan struct{}),
 	}
 	s.wg.Add(1)
@@ -59,23 +62,37 @@ func (s *Store) Close() {
 	s.wg.Wait()
 }
 
+// Events returns the event channel.
+// The app reads this channel and applies the events to the UI.
 func (s *Store) Events() <-chan Event {
 	return s.eventsOut
 }
 
+// AddItem tells the store to add a new item.
 func (s *Store) AddItem(item Item) {
-	s.handleEvent(&ItemAdded{ID: randomID(), Item: item})
+	s.enqueueInputEvent(&ItemAdded{ID: randomID(), Item: item})
 }
 
+// RemoveItem tells the store to delete an item.
 func (s *Store) RemoveItem(id ID) {
-	s.handleEvent(&ItemRemoved{ID: id})
+	s.enqueueInputEvent(&ItemRemoved{ID: id})
 }
 
+// UpdateItem tells the store to change an item.
 func (s *Store) UpdateItem(id ID, item Item) {
-	s.handleEvent(&ItemChanged{ID: id, Item: item})
+	s.enqueueInputEvent(&ItemChanged{ID: id, Item: item})
 }
 
-func (s *Store) handleEvent(ev Event) {
+// Persist tells the store to flush data to disk.
+func (s *Store) Persist() {
+	select {
+	case s.flushCh <- struct{}{}:
+	default:
+	}
+}
+
+// enqueueInputEvent delivers an event from the app to mainLoop.
+func (s *Store) enqueueInputEvent(ev Event) {
 	select {
 	case s.eventsIn <- ev:
 	case <-s.quitCh:
@@ -91,20 +108,33 @@ func (s *Store) mainLoop() {
 		s.enqueueOutputEvent(&IOError{Err: err})
 	}
 
+	// Handle events.
 	for {
 		sendEvChan, sendEv := s.queuedOutputEvent()
 		select {
 		case sendEvChan <- sendEv:
 			s.popOutputEvent()
+
 		case ev := <-s.eventsIn:
 			if err := s.writeEvent(ev); err != nil {
 				s.enqueueOutputEvent(&IOError{Err: err})
 			} else {
 				s.enqueueOutputEvent(ev)
 			}
+
+		case <-s.flushCh:
+			if s.dataFile != nil {
+				err := s.dataFile.Sync()
+				log.Printf("data file flushed (err: %v)", err)
+				if err != nil {
+					s.enqueueOutputEvent(&IOError{Err: err})
+				}
+			}
+
 		case <-s.quitCh:
 			if s.dataFile != nil {
-				s.dataFile.Close()
+				err := s.dataFile.Close()
+				log.Printf("data file closed (err: %v)", err)
 			}
 			return
 		}
@@ -112,30 +142,26 @@ func (s *Store) mainLoop() {
 }
 
 func (s *Store) enqueueOutputEvent(ev Event) {
-	s.eventQueue = append(s.eventQueue, ev)
+	s.eventQueue.PushBack(ev)
 }
 
 func (s *Store) queuedOutputEvent() (chan Event, Event) {
-	if len(s.eventQueue) == 0 {
+	first := s.eventQueue.Front()
+	if first == nil {
 		return nil, nil
 	}
-	return s.eventsOut, s.eventQueue[0]
+	return s.eventsOut, first.Value.(Event)
 }
 
 func (s *Store) popOutputEvent() {
-	// log.Printf("sent event: %#v", s.eventQueue[0])
-	copy(s.eventQueue, s.eventQueue[1:])
-	s.eventQueue = s.eventQueue[:len(s.eventQueue)-1]
+	s.eventQueue.Remove(s.eventQueue.Front())
 }
 
 func (s *Store) writeEvent(ev Event) error {
 	if err := s.initFile(); err != nil {
 		return err
 	}
-	if err := writeEvent(s.writer, ev); err != nil {
-		return err
-	}
-	return s.dataFile.Sync()
+	return writeEvent(s.writer, ev)
 }
 
 func (s *Store) initFile() error {
@@ -151,7 +177,7 @@ func (s *Store) initFile() error {
 	if err != nil {
 		return err
 	}
-	log.Printf("file opened: %s", filename)
+	log.Printf("data file opened: %s", filename)
 	s.dataFile = f
 	s.reader = json.NewDecoder(f)
 	s.writer = json.NewEncoder(f)
