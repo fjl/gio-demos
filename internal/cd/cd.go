c1a58f3d3a22b0f8369e9fe345485c8703e7da67
Felix Lange <fjl@twurst.com>
2023-03-24 13:13:29 +0100

    internal/cd: new package for C, D type aliases

---
+++ internal/cd/cd.go
@@ -0,0 +1,10 @@
+// Package cd provides aliases for common Gio types.
+// It is meant to be dot-imported.
+package cd
+
+import "gioui.org/layout"
+
+type (
+	C = layout.Context
+	D = layout.Dimensions
+)
